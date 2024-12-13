a84e5fd451e3735154999959e4fc278fc79d3da6
Khanhhnse183037 <huynhngockhanh1201@gmail.com>
2024-12-13 15:36:02 +0700

    f

--- assets/js/home.js
+++ assets/js/home.js
@@ -59,7 +59,7 @@ fetch(`${API_URL}/news/latestnews?quantity=${quantity}`)
         });
     });
 
-fetch(`${API_URL}/product/newproduct`)
+fetch(`https://localhost:7232/product/newproduct`)
     .then(function(res){
         return res.json();
     })
